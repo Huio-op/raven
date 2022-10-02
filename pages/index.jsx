b33d809b2a4a0713b724ec36af32fa2f32e87999
EnzoWM02 <enzowm02@gmail.com>
2022-10-02 01:22:29 -0300

    #7 Correções Prisma e banco de dados, ajuste no read.me

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,50 +1,30 @@
 import React from "react"
-import prisma from "../lib/prisma";
+import { useState } from "react";
+import { useEffect } from "react";
 import Layout from "../components/Layout"
-import Post from "../components/Post"
-
-export const getStaticProps = async () => {
-  const feed = await prisma.post.findMany({
-    where: {published: true},
-    include: {
-      author: {
-        select: {name: true},
-      }
-    }
-  });
-  return { 
-    props: { feed }, 
-    revalidate: 10 
-  }
-}
 
 const Blog = (props) => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data)
+      })
+  }, [])
+
+  console.log(posts);
+
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
         <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
+          enzo
         </main>
       </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
     </Layout>
   )
 }
